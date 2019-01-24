1a19ebd85197c239ddfb93c88826cae92b7baa99
bassambst <bassambst@gmail.com>
2019-01-24 23:28:15 +0800

    test connection

---
+++ index.js
@@ -0,0 +1,30 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+
+// Create connection to database
+var config = {
+  server: '127.0.0.1',
+  options: {
+      instanceName: 'SQLEXPRESS',
+      database: 'SampleDB',
+      encrypt: true
+  },
+  authentication: {
+      type: "default",
+      options: {
+        userName: "sa",
+        password: "1"
+      }
+  }
+}
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected');
+  }
+});
